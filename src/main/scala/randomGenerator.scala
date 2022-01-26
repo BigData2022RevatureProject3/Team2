1a034d18e1ba010eb4e430b9066516707e488e52
Michael Armstrong <armstrongm270x@icloud.com>
2022-01-26 15:43:10 -0500

    Created Approx 4% Chance Of Error. Runs Random Function To Cause Error.

--- src/main/scala/randomGenerator.scala
+++ src/main/scala/randomGenerator.scala
@@ -1,34 +1,36 @@
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.sql.{DataFrame, SparkSession}
-
 import java.io.FileNotFoundException
 import scala.collection.mutable.{ArrayBuffer, ListBuffer}
 import scala.util.Random
 
 object randomGenerator {
   //Tested for file not found
-  Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
-  Logger.getLogger("org.spark-project").setLevel(Level.ERROR)
-  Logger.getLogger("org").setLevel(Level.ERROR);
-  val spark: SparkSession = SparkSession
-    .builder
-    .appName("Covid Analyze App")
-    .config("spark.master", "local[*]")
-    .enableHiveSupport()
-    .getOrCreate()
-  spark.sparkContext.setLogLevel("ERROR")
-  val products: DataFrame = spark.read.option("header", "true").csv("data/products.csv")
-  val names: DataFrame = spark.read.option("header", "true").csv("data/customer_names.csv")
-  val websites: DataFrame = spark.read.option("header", "true").csv("data/ecommerce_websites.csv")
-  val failures: DataFrame = spark.read.option("header", "true").csv("data/failure_list.csv")
-  val locations: DataFrame = spark.read.option("header", "true").csv("data/Countries_Cities.csv")
-  val name_cities: ArrayBuffer[String] = randomize_cities()
-  var count = 1
-
   def main(args: Array[String]): Unit = {
-    generate_2()
-  }
+    Logger.getLogger("org.apache.spark").setLevel(Level.ERROR)
+    Logger.getLogger("org.spark-project").setLevel(Level.ERROR)
+    Logger.getLogger("org").setLevel(Level.ERROR)
 
+    val spark : SparkSession = SparkSession
+      .builder
+      .appName("Covid Analyze App")
+      .config("spark.master", "local[*]")
+      .enableHiveSupport()
+      .getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    try{
+      val products = spark.read.option("header","true").csv("data/products.csv")
+      val names = spark.read.option("header","true").csv("data/customer_names.csv")
+      val websites = spark.read.option("header","true").csv("data/ecommerce_websites.csv")
+      val failures = spark.read.option("header","true").csv("data/failure_list.csv")
+      val locations = spark.read.option("header","true").csv("data/Countries_Cities.csv")
+      generate(products,failures,locations,websites,names)
+    }
+    catch {
+      case e:Exception => println("File not found")
+    }
+  }
 
   def generate_2(): Unit = {
     var output = List[String]()
@@ -66,6 +68,56 @@ object randomGenerator {
   private var _transactionID: Long = 0
 
   def getNextTransactionID: Long = {
+
+  // Routing For Bad Data
+  def bad_data(modArr: String): String = {
+    val ranOpt = Random.nextInt(4)
+    val newArr = modArr.split(",")
+
+    val moddedArr = ranOpt match {
+      case 0 => mismatched_name(newArr)
+      case 1 => false_failure(newArr, "False Failure")
+      case 2 => negative_price(newArr)
+      case 3 => random_null(newArr, Random.nextInt(newArr.length))
+    }
+
+    return moddedArr.mkString(",")
+  }
+
+  // Swap Customer And Product Name
+  def mismatched_name(modName: Array[String]): Array[String] = {
+    println("Mismatched Name")
+    return modName
+  }
+
+  // False Payment Failure
+  def false_failure(modFail: Array[String], errMsg: String): Array[String] = {
+    val resArray = modFail
+
+    if(modFail(modFail.length - 1) == "Y")
+    {resArray(5) = ""}
+    else
+    {resArray(5) = errMsg}
+
+    return resArray
+  }
+
+  // Negative Product Price
+  def negative_price(modPrice: Array[String]): Array[String] = {
+    val newPrice = modPrice(3).toDouble - (modPrice(3).toDouble * 2)
+    modPrice(3) = newPrice.toString
+    return modPrice
+  }
+
+  // Null Value In Random Field
+  def random_null(modNull: Array[String], index: Int): Array[String] = {
+    modNull(index) = ""
+    return modNull
+  }
+
+  //Michael
+  private var _transactionID : Long = 0
+  def getNextTransactionID : Long = {
     _transactionID += 1
     _transactionID
   }
@@ -83,6 +135,7 @@ object randomGenerator {
     if (_reasons.length < 2) {
       _reasons = failures.collect().map(_.getString(0))
     }
+
     _reasons(Random.nextInt(_reasons.length))
   }
 
@@ -103,9 +156,11 @@ object randomGenerator {
   //Mike
   def pull_cities_countries(locations: DataFrame): Array[String] = {
     val locationRes = new Array[String](2)
+
     try {
       val df = locations.collect()
       val rIndex = Random.nextInt(df.length)
+
       locationRes(0) = df(rIndex)(1).toString
       locationRes(1) = df(rIndex)(0).toString
 
@@ -114,6 +169,7 @@ object randomGenerator {
     catch {
       case e => println("File Not Found")
     }
+
     locationRes
   }
 
--- src/test/scala/ProducerTest.scala
+++ src/test/scala/ProducerTest.scala
@@ -1,5 +1,20 @@
 import org.scalatest._
+import com.badDataGenerator.{false_failure, negative_price, random_null}
 
 class ProducerTest extends flatspec.AnyFlatSpec with matchers.must.Matchers {
+  "random_null()" should "Return Null And Index" in {
+    val startArray = Array[String]("800050", "Kimchi", "Food", "3.99", "68", "Payment gateway unreachable", "Frankfurt", "Germany", "68", "UPI", "N")
+    assert(random_null(startArray, 3)(3) == "")
+  }
 
+  "negative_price()" should "Return Negative Inverse Of N" in {
+    val startArray = Array[String]("800050", "Kimchi", "Food", "5485.99", "68", "Payment gateway unreachable", "Frankfurt", "Germany", "68", "UPI", "N")
+    assert(negative_price(startArray)(3) == "-5485.99")
+  }
+
+  "false_failure()" should "Return False Error Or Null" in {
+    val resArray = Array[String]("800050", "Kimchi", "Food", "3.99", "68", "", "Frankfurt", "Germany", "68", "UPI", "N")
+    val postArray = Array[String]("800050", "Kimchi", "Food", "3.99", "68", "False Failure", "Frankfurt", "Germany", "68", "UPI", "N")
+    assert(false_failure(resArray, "False Failure") === postArray)
+  }
 }
