591fefc61d091ecd2329ff124988982957831906
Jaime Hinojos <jaimehinojos11@gmail.com>
2022-02-01 15:01:42 -0700

    Looped producer for multiple entries and 1 second delay also edited random null method in random generator file

--- src/main/scala/Producer.scala
+++ src/main/scala/Producer.scala
@@ -32,27 +32,31 @@ object Producer {
     props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer")
     props.put("acks", "all")
 
-    val batch = rand.generate_2()
-    println("done")
-    val producer = new KafkaProducer[String, String](props)
-    val topic = "team2"
 
-    try {
-      var count = 0
-      batch.foreach(x => {
-        val record = new ProducerRecord[String, String](topic, count.toString, x)
-        val metadata = producer.send(record)
-        printf(s"sent record(key=%s value=%s) " +
-          "meta(partition=%d, offset=%d)\n",
-          record.key(), record.value(),
-          metadata.get().partition(),
-          metadata.get().offset())
-        count += 1
-      })
-    } catch {
-      case e: Exception => e.printStackTrace()
-    } finally {
-      producer.close()
+    for(i <- 0 until 5){
+      val batch = rand.generate_2(i*100 + 1)
+      val producer = new KafkaProducer[String, String](props)
+      val topic = "team2"
+
+      try {
+        var count = 0
+        batch.foreach(x => {
+          val record = new ProducerRecord[String, String](topic, count.toString, x)
+          val metadata = producer.send(record)
+          printf(s"sent record(key=%s value=%s) " +
+            "meta(partition=%d, offset=%d)\n",
+            record.key(), record.value(),
+            metadata.get().partition(),
+            metadata.get().offset())
+          count += 1
+        })
+        Thread.sleep(1000)
+      } catch {
+        case e: Exception => e.printStackTrace()
+      } finally {
+        producer.close()
+      }
     }
+
   }
 }
